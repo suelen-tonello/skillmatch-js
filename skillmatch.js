06b4de641244d76e149c4530a74c2ee8653acf31
Suelen Tonello <suelentonello85@gmail.com>
2026-05-19 14:21:07 -0300

    feat: adicionado dados do candidato

---
+++ skillmatch.js
@@ -0,0 +1,14 @@
+const candidato = {
+  nome: "Suelen Tonello",
+  area: "Front-End",
+  habilidades: [
+    "JavaScript",
+    "GitHub",
+    "Logica de Programacao",
+    "Kanban",
+    "HTML",
+    "CSS",
+  ],
+  experienciaMeses: 10,
+  aceitaModeloHibrido: true,
+};
